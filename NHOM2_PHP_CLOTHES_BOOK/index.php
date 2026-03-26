b27e7bba79d8fcf7a0c7473937cb173731c0b1e3
Khánh Duy <105479692+khanh-duy-003@users.noreply.github.com>
2026-03-26 13:12:39 +0700

    Update index.php

--- NHOM2_PHP_CLOTHES_BOOK/index.php
+++ NHOM2_PHP_CLOTHES_BOOK/index.php
@@ -1,6 +1,11 @@
 <?php
-session_start();
-require_once ('vendor/autoload.php');
-require_once ('config/database.php');
-use Src\Route;
-Route::route_site();
+// session_start();
+// require_once ('vendor/autoload.php');
+// require_once ('config/database.php');
+// use Src\Route;
+// Route::route_site();
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "APP START"; die();
